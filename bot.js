bd7daeb4cf25146bbd28272ed08eef320410563d
SaTAN002 <45322453+SaTAN002@users.noreply.github.com>
2018-11-25 03:52:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag} !);
+  console.log(`Logged in as ${client.user.tag} !`);
  
 });
  
@@ -15,16 +15,16 @@ client.on('message', message => {
   if (message.author.id !== '360095764535312385') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(**✅  : ${argresult}**)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 } else 
 
 if (message.content.startsWith(PrEfix + 'ls')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(**✅  : ${argresult}**)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 } else 
 if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/kokoseda%22");
-    message.channel.sendMessage(**✅  : ${argresult}**)
+  client.user.setGame(argresult, "https://www.twitch.tv/kokoseda");
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 }
 
 });
